b91718d6bcf2ee611ff5ddc30c6432235bdbfca2
realdmitrii <conceptproect@gmail.com>
2022-10-22 03:01:54 +0500

    redeyes

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
-app.use(express.static('./'));
+app.use(express.static(__dirname + '/dist'));
 
 app.listen(PORT, function () {
   console.log(`Messenger app listening on port ${PORT}!`);
